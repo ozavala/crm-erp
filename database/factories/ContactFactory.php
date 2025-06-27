637ff16aed19d1a2e37f1fb543608405146add60
ozavala <oszavala@gmail.com>
2025-06-27 13:25:39 -0500

    feat: Refactor ContactFactory for improved faker usage and add QuotationFactory skeleton

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -3,24 +3,28 @@
 namespace Database\Factories;
 
 use App\Models\Contact;
-use App\Models\Customer;
 use App\Models\CrmUser;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
 class ContactFactory extends Factory
 {
-   
-
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition(): array
     {
         return [
-            
-            'first_name' => $this->faker->firstName,
-            'last_name' => $this->faker->lastName,
-            'email' => $this->faker->unique()->safeEmail,
-            'phone' => $this->faker->phoneNumber,
-            'title' => $this->faker->jobTitle,
-            'created_by_user_id' => CrmUser::factory(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'title' => $this->faker->jobTitle(),
+            'created_by_user_id' => CrmUser::inRandomOrder()->first()->user_id ?? CrmUser::factory(),
         ];
     }
 }
